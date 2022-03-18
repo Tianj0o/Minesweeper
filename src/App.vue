a1d4611a79c572070b3e563fd98c16b6b68e7468
Tianj0o <2576099053@qq.com>
2022-03-18 21:37:28 +0800

    finish ui

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,32 @@
 import iconSun from './components/icons/iconSun.vue'
 import iconMoon from './components/icons/iconMoon.vue'
 import { useDarkMode } from './hooks/useDarkMode'
+import { ref } from '@vue/runtime-core';
+import IconBomb from './components/icons/iconBomb.vue';
 const mode = useDarkMode()
+interface block {
+  x: number,
+  y: number,
+  isMine: boolean
+  isOpen: boolean
+}
+let arr: block[][] = new Array(5)
+
+arr = Array.from({ length: 10 }, (_, y) =>
+  Array.from({ length: 10 }, (_, x) =>
+  ({
+    x,
+    y,
+    isMine: true,
+    isOpen: false
+  }))
+)
+
+const blocks = ref(arr)
+const handleClick = (block: block) => {
+  block.isOpen = !block.isOpen
+}
+let status = ref('dev')
 
 </script>
 
@@ -13,9 +38,30 @@ const mode = useDarkMode()
       <icon-sun v-if="mode === 'auto'" />
       <icon-moon v-else />
     </div>
+    <div class="flex flex-col gap-0.5">
+      <template v-for="line in blocks">
+        <div class="flex gap-0.5">
+          <template v-for="block in line">
+            <button
+              @click="handleClick(block)"
+              class="w-8 h-8 lg:w-10 lg:h-10 border border-gray-400 flex items-center justify-center"
+              :class="'open'"
+            >
+              <!-- {{ block.x + 1 + block.y * 10 }} -->
+              <icon-bomb v-if="(block.isOpen || status === 'dev') && block.isMine" />
+            </button>
+          </template>
+        </div>
+      </template>
+    </div>
+    <button @click="status = status === 'dev' ? 'dep' : 'dev'">{{ status }}</button>
   </div>
 </template>
 
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 </style>
